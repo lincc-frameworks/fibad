d92ffff030751874e94ee712e41739a6f6249451
Michael Tauraso <mtauraso@users.noreply.github.com>
2024-08-16 15:14:10 -0700

    Scaffolding for Fibad library instance object (#30)
    * All current verbs use a unified interface verb.run(config) with no args passed
    * New Fibad object holds the fully merged config, hands it out to verbs
    * CLI uses Fibad object to invoke verbs.
    * fibad_default_config.toml is now part of the fibad package and is loaded as a default config always

--- src/fibad/train.py
+++ src/fibad/train.py
@@ -1,30 +1,24 @@
 import torch
 
-from fibad.config_utils import get_runtime_config
 from fibad.data_loaders.data_loader_registry import fetch_data_loader_class
 from fibad.models.model_registry import fetch_model_class
 
 
-def run(args, config):
+def run(config):
     """Placeholder for training code.
 
     Parameters
     ----------
-    args : argparse.Namespace
-        The parsed command line arguments.
-
     config : dict
         The parsed config file as a nested
         dict
     """
 
-    runtime_config = get_runtime_config(args.runtime_config)
+    model_cls = fetch_model_class(config)
+    model = model_cls(config.get("model", {}))
 
-    model_cls = fetch_model_class(runtime_config)
-    model = model_cls(runtime_config.get("model", {}))
-
-    data_loader_cls = fetch_data_loader_class(runtime_config)
-    data_loader = data_loader_cls(runtime_config.get("data_loader", {})).get_data_loader()
+    data_loader_cls = fetch_data_loader_class(config)
+    data_loader = data_loader_cls(config.get("data_loader", {})).get_data_loader()
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     if torch.cuda.device_count() > 1:
--- src/fibad_cli/main.py
+++ src/fibad_cli/main.py
@@ -1,48 +1,24 @@
 import argparse
-import importlib
 import sys
 from importlib.metadata import version
 
-# TODO config system, for now edit the dict
-
-download_config = {
-    "sw": "22asec",
-    "sh": "22asec",
-    "filter": ["HSC-G", "HSC-R", "HSC-I", "HSC-Z", "HSC-Y"],
-    "type": "coadd",
-    "rerun": "pdr3_wide",
-    "username": "mtauraso@local",
-    "password": "cCw+nX53lmNLHMy+JbizpH/dl4t7sxljiNm6a7k1",
-    "max_connections": 2,
-    "fits_file": "../hscplay/temp.fits",
-    "cutout_dir": "../hscplay/cutouts/",
-    "offset": 0,
-    "num_sources": 10,
-}
-
-config = {"download": download_config}
+from fibad import Fibad
 
 
 def main():
     """Primary entry point for the Fibad CLI. This handles dispatching to the various
-    Fibad actions. The actions are defined in the pyproject.toml project.scripts
-    section.
+    Fibad actions.
     """
 
     description = "Fibad CLI"
     epilog = "FIBAD is the Framework for Image-Based Anomaly Detection"
-
-    #! We could potentially make this dynamic
-    #! Somewhat difficult (perhaps impossible) to get this list from importlib/pkglib given
-    #! That a fibad verb is simply an object in a file.
-    fibad_verbs = ["train", "predict", "download"]
 
     parser = argparse.ArgumentParser(description=description, epilog=epilog)
 
     parser.add_argument("--version", dest="version", action="store_true", help="Show version")
     parser.add_argument("-c", "--runtime-config", type=str, help="Full path to runtime config file")
 
-    parser.add_argument("verb", nargs="?", choices=fibad_verbs, help="Verb to execute")
+    parser.add_argument("verb", nargs="?", choices=Fibad.verbs, help="Verb to execute")
 
     args = parser.parse_args()
 
@@ -56,14 +32,8 @@ def main():
         parser.print_help()
         sys.exit(1)
 
-    fibad_action = f"fibad-{args.verb}"
-
-    # Ensure the action is available
-    if args.verb not in fibad_verbs:
-        print(f"Error: '{fibad_action}' is not available. Available actions are : {', '.join(fibad_verbs)}")
-        sys.exit(1)
-
-    importlib.import_module(f"fibad.{args.verb}").run(args, config)
+    fibad_instance = Fibad(config_file=args.runtime_config)
+    getattr(fibad_instance, args.verb)()
 
 
 if __name__ == "__main__":
