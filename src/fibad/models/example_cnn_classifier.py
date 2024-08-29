8501d953d957ed7dc57c20de414f2f9af735bae1
drewoldag <47493171+drewoldag@users.noreply.github.com>
2024-08-29 14:39:28 -0700

    Basic functionality to be tested on computer with GPU/Klone.

--- src/fibad/models/example_cnn_classifier.py
+++ src/fibad/models/example_cnn_classifier.py
@@ -27,6 +27,11 @@ class ExampleCNN(nn.Module):
 
         self.config = model_config
 
+        # Optimizer and criterion could be set directly, i.e. `self.optimizer = optim.SGD(...)`
+        # but we define them as methods as a way to allow for more flexibility in the future.
+        self.optimizer = self._optimizer()
+        self.criterion = self._criterion()
+
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
@@ -36,38 +41,28 @@ class ExampleCNN(nn.Module):
         x = self.fc3(x)
         return x
 
-    def train(self, trainloader, device=None):
-        self.optimizer = self._optimizer()
-        self.criterion = self._criterion()
+    def train_step(self, batch):
+        """This function contains the logic for a single training step. i.e. the
+        contents of the inner loop of a ML training process.
 
-        for epoch in range(self.config.get("epochs", 2)):
-            running_loss = 0.0
-            for i, data in enumerate(trainloader, 0):
-                inputs, labels = data
-                inputs, labels = inputs.to(device), labels.to(device)
+        Parameters
+        ----------
+        batch : tuple
+            A tuple containing the inputs and labels for the current batch.
 
-                self.optimizer.zero_grad()
-                outputs = self(inputs)
-                loss = self.criterion(outputs, labels)
-                loss.backward()
-                self.optimizer.step()
-                running_loss += loss.item()
-                if i % 2000 == 1999:
-                    logger.info(f"[{epoch + 1}, {i + 1}] loss: {running_loss / 2000}")
-                    running_loss = 0.0
-
-    # Creating a train_step function to be used with pytorch-ignite
-    # ! figure out how to pass `device` correctly!!! It shouldn't be in the method signature
-    # ! I just put it there to pass linting.
-    def train_step(self, batch, device):
+        Returns
+        -------
+        Current loss value
+            The loss value for the current batch.
+        """
         inputs, labels = batch
-        inputs, labels = inputs.to(device), labels.to(device)
 
         self.optimizer.zero_grad()
         outputs = self(inputs)
         loss = self.criterion(outputs, labels)
         loss.backward()
         self.optimizer.step()
+        return loss.item()
 
     def _criterion(self):
         return nn.CrossEntropyLoss()
--- src/fibad/train.py
+++ src/fibad/train.py
@@ -1,6 +1,7 @@
 import logging
 
 import ignite.distributed as idist
+from ignite.contrib.handlers import ProgressBar
 from ignite.engine import Engine
 
 from fibad.data_loaders.data_loader_registry import fetch_data_loader_class
@@ -10,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 
 def run(config):
-    """Placeholder for training code.
+    """Run the training process for a given model and data loader.
 
     Parameters
     ----------
@@ -19,30 +20,55 @@ def run(config):
         dict
     """
 
+    # Fetch data loader class specified in config and create an instance of it
     data_loader_cls = fetch_data_loader_class(config)
-    fibad_data_loader = data_loader_cls(config.get("data_loader", {}))
-    data_set = fibad_data_loader.data_set()
-    data_loader = _train_data_loader(data_set, config.get("data_loader", {}))
+    data_loader = data_loader_cls(config.get("data_loader", {}))
 
+    # Get the pytorch.dataset from dataloader, and use it to create a distributed dataloader
+    data_set = data_loader.data_set()
+    dist_data_loader = _train_data_loader(data_set, config.get("data_loader", {}))
+
+    # Fetch model class specified in config and create an instance of it
     model_cls = fetch_model_class(config)
-    model = model_cls(model_config=config.get("model", {}), shape=fibad_data_loader.shape())
+    model = model_cls(model_config=config.get("model", {}), shape=data_loader.shape())
 
-    model = idist.auto_model(model)
+    # Create trainer, a pytorch-ignite `Engine` object
+    trainer = _create_trainer(model)
 
-    trainer = Engine(model.train_step)
-    trainer.run(data_loader, max_epochs=config.get("model", {}).get("epochs", 2))
+    # Run the training process
+    trainer.run(dist_data_loader, max_epochs=config.get("model", {}).get("epochs", 2))
 
+    # Save the trained model
+    model.save()
     logger.info("Finished Training")
 
 
 def _train_data_loader(data_set, config):
     # ~ idist.auto_dataloader will accept a **kwargs parameter, and pass values
     # ~ through to the underlying pytorch DataLoader.
+    # ~ Currently, our config includes unexpected keys like `name`, that cause
+    # ~ an exception. It would be nice to reduce this to:
+    # ~ `data_loader = idist.auto_dataloader(data_set, **config)`
     data_loader = idist.auto_dataloader(
         data_set,
         batch_size=config.get("batch_size", 4),
         shuffle=config.get("shuffle", True),
-        drop_last=config.get("drop_last", False),
+        num_workers=config.get("num_workers", 2),
     )
 
     return data_loader
+
+
+def _create_trainer(model):
+    device = idist.device()
+    print(f"Working with device: {device}")
+    model = idist.auto_model(model)
+
+    def train_step(engine, batch):
+        batch = tuple(i.to(device) for i in batch)
+        model.train_step(engine, batch)
+
+    trainer = Engine(train_step)
+    ProgressBar().attach(trainer)
+
+    return trainer
