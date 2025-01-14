7b2e5dec11912247ca79afa08a792e1f2a81f2ae
Aritra Ghosh <aritraghsh09@gmail.com>
2025-01-13 16:49:03 -0800

    initial commit of DCAE that works on HSC data

--- src/fibad/models/__init__.py
+++ src/fibad/models/__init__.py
@@ -1,6 +1,8 @@
 from .example_autoencoder import ExampleAutoencoder
 from .example_cnn_classifier import ExampleCNN
+from .example_hsc_autoencoder import HSCAutoencoder
+from .hsc_dcae import HSCDCAE
 from .hsc_autoencoder import HSCAutoencoder
 from .model_registry import MODEL_REGISTRY, fibad_model
 
-__all__ = ["fibad_model", "MODEL_REGISTRY", "ExampleCNN", "ExampleAutoencoder", "HSCAutoencoder"]
+__all__ = ["fibad_model", "MODEL_REGISTRY", "ExampleCNN", "ExampleAutoencoder", "HSCAutoencoder", "HSCDCAE"]
