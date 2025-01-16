3a4a2881c30ec9656acebe8a1a4e76b643ce4602
Drew Oldag <awoldag@uw.edu>
2025-01-15 16:48:52 -0800

    I used .get on a config when I should not have. Fixing that here.

--- src/fibad/models/model_registry.py
+++ src/fibad/models/model_registry.py
@@ -30,7 +30,10 @@ def _torch_criterion(self: nn.Module):
     # Load the class and get any parameters from the config dictionary
     criterion_cls = get_or_load_class(self.config["criterion"])
     criterion_name = self.config["criterion"]["name"]
-    arguments = self.config.get(criterion_name, {})
+
+    arguments = {}
+    if criterion_name in self.config:
+        arguments = self.config[criterion_name]
 
     # Print some information about the criterion function and parameters used
     log_string = f"Using criterion: {criterion_name} "
@@ -50,7 +53,10 @@ def _torch_optimizer(self: nn.Module):
     # Load the class and get any parameters from the config dictionary
     optimizer_cls = get_or_load_class(self.config["optimizer"])
     optimizer_name = self.config["optimizer"]["name"]
-    arguments = self.config.get(optimizer_name, {})
+
+    arguments = {}
+    if optimizer_name in self.config:
+        arguments = self.config[optimizer_name]
 
     # Print some information about the optimizer function and parameters used
     log_string = f"Using optimizer: {optimizer_name} "
