58aa535e7860dcd9933253daa4db80e7f3f41103
Drew Oldag <awoldag@uw.edu>
2025-02-05 16:05:50 -0800

    Attempting to introduce enough typing in module_registry that mypy in pre-commit github actions will pass.

--- src/fibad/data_sets/__init__.py
+++ src/fibad/data_sets/__init__.py
@@ -3,4 +3,10 @@ from .example_cifar_data_set import CifarDataSet
 from .hsc_data_set import HSCDataSet
 from .inference_dataset import InferenceDataSet
 
-__all__ = ["fibad_data_set", "DATA_SET_REGISTRY", "CifarDataSet", "HSCDataSet"]
+__all__ = [
+    "fibad_data_set",
+    "DATA_SET_REGISTRY",
+    "CifarDataSet",
+    "HSCDataSet",
+    "InferenceDataSet",
+]
