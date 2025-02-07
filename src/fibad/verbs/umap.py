6a6a2419ce6622114fb56a3a7091772ae6e8e314
Aritra Ghosh <aritraghsh09@gmail.com>
2025-02-06 18:27:53 -0800

    Moves UMAP kwargs to config file, streamlines code, adds progress bar

--- src/fibad/verbs/umap.py
+++ src/fibad/verbs/umap.py
@@ -1,4 +1,3 @@
-import inspect
 import logging
 import pickle
 from argparse import ArgumentParser, Namespace
@@ -7,6 +6,7 @@ from typing import Optional, Union
 
 import numpy as np
 import umap
+from tqdm.auto import tqdm
 
 from fibad.config_utils import create_results_dir
 from fibad.data_sets.inference_dataset import InferenceDataSet, InferenceDataSetWriter
@@ -34,19 +34,6 @@ class Umap(Verb):
             help="Directory containing inference results to umap.",
         )
 
-        # Get all parameters from UMAP's constructor dynamically
-        # and allow users to pass values for these.
-        umap_params = inspect.signature(umap.UMAP).parameters
-
-        for param, details in umap_params.items():
-            if details.default is not inspect.Parameter.empty:  # Add only those with defaults
-                parser.add_argument(
-                    f"--{param}",
-                    type=type(details.default),
-                    default=details.default,
-                    help=f"UMAP parameter: {param}",
-                )
-
     # Should there be a version of this on the base class which uses a dict on the Verb
     # superclass to build the call to run based on what the subclass verb defined in setup_parser
     def run_cli(self, args: Optional[Namespace] = None):
@@ -55,18 +42,32 @@ class Umap(Verb):
         if args is None:
             raise RuntimeError("Run CLI called with no arguments.")
 
-        # Get valid UMAP parameters and extract those args to pass to umap
-        valid_umap_params = set(inspect.signature(umap.UMAP).parameters.keys())
-        umap_params = {k: v for k, v in vars(args).items() if k in valid_umap_params and v is not None}
-
         # This is where we map from CLI parsed args to a
         # self.run (args) call.
-        return self.run(input_dir=args.input_dir, **umap_params)
+        return self.run(input_dir=args.input_dir)
 
-    def run(self, input_dir: Optional[Union[Path, str]] = None, **kwargs):
-        """Create a umap of a particular inference run"""
+    def run(self, input_dir: Optional[Union[Path, str]]):
+        """
+        Create a umap of a particular inference run
 
-        reducer = umap.UMAP(**kwargs)
+        This method loads the latent space representations from an inference run,
+        samples a subset of data points, flattens them if necessary, and then fits
+        a UMAP model. The fitted reducer is then used to transform the entire dataset
+        into a lower-dimensional space.
+
+        Parameters
+        ----------
+        input_dir : str or Path, Optional
+            The directory containing the inference results.
+
+
+        Returns
+        -------
+        None
+            The method does not return anything but saves the UMAP representations to disk.
+        """
+
+        reducer = umap.UMAP(**self.config["umap.UMAP"])
 
         # Set up the results directory where we will store our umapped output
         results_dir = create_results_dir(self.config, "umap")
@@ -82,15 +83,9 @@ class Umap(Verb):
         sample_size = np.min([config_sample_size if config_sample_size else np.inf, total_length])
         rng = np.random.default_rng()
         index_choices = rng.choice(np.arange(total_length), size=sample_size, replace=False)
-        data_sample = inference_results[index_choices].numpy()
 
-        reshape = False
-        if data_sample.ndim != 2:
-            msg = f"The arrays passed to umap have dimensions {data_sample.shape[1:]}."
-            msg += "These will be flattened before being passed to umap"
-            logger.info(msg)
-            data_sample = data_sample.reshape(data_sample.shape[0], -1)
-            reshape = True
+        # If the input to umap is not of the shape [samples,input_dims] we reshape the input accordingly
+        data_sample = inference_results[index_choices].numpy().reshape(sample_size, -1)
 
         # Fit a single reducer on the sampled data
         reducer.fit(data_sample)
@@ -105,10 +100,12 @@ class Umap(Verb):
 
         all_indexes = np.arange(0, total_length)
         all_ids = np.array([int(i) for i in inference_results.ids()])
-        for batch_indexes in np.array_split(all_indexes, num_batches):
-            batch = inference_results[batch_indexes]
-            if reshape:
-                batch = batch.reshape(batch.shape[0], -1)
+        for batch_indexes in tqdm(
+            np.array_split(all_indexes, num_batches),
+            desc="Creating Lower Dimensional Representation using UMAP",
+            total=num_batches,
+        ):
+            batch = inference_results[batch_indexes].reshape(len(batch_indexes), -1)
             batch_ids = all_ids[batch_indexes]
             transformed_batch = reducer.transform(batch)
             umap_results.write_batch(batch_ids, transformed_batch)
