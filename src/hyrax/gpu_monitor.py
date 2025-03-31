28d38290c81267500b5129b7b600b3e99ad11f48
Drew Oldag <awoldag@uw.edu>
2025-03-30 22:04:45 -0700

    Removed dependency on GPUtil. Copied over the relevant code from GPUtil and parsed it down to only what is needed for Hyrax.

--- src/hyrax/gpu_monitor.py
+++ src/hyrax/gpu_monitor.py
@@ -1,7 +1,8 @@
+import os
 import time
+from dataclasses import dataclass
+from subprocess import Popen, PIPE
 from threading import Thread
-
-import GPUtil
 
 
 class GpuMonitor(Thread):
@@ -20,7 +21,7 @@ class GpuMonitor(Thread):
     def run(self):
         """Run loop that logs GPU metrics every `self.delay` seconds."""
         while not self.stopped:
-            gpus = GPUtil.getGPUs()
+            gpus = getGPUs()
             step = time.time() - self.start_time
             for gpu in gpus:
                 gpu_name = f"GPU_{gpu.id}"
@@ -33,3 +34,62 @@ class GpuMonitor(Thread):
     def stop(self):
         """Stop the monitoring thread."""
         self.stopped = True
+
+
+"""The following is based on GPUtil. It has been striped down to only the
+functionality needed for Hyrax. The original code can be found here:
+https://github.com/anderskm/gputil
+"""
+@dataclass
+class GPU:
+    """Holds the GPU metrics retrieved from nvidia-smi."""
+    id: int
+    load: float
+    memoryTotal: float
+    memoryUsed: float
+
+    @property
+    def memoryUtil(self):
+        """Return the memory utilization of the GPU."""
+        return self.memoryUsed / self.memoryTotal
+
+
+def safeFloatCast(strNumber):
+    try:
+        number = float(strNumber)
+    except ValueError:
+        number = float('nan')
+    return number
+
+
+def getGPUs():
+    """Get the GPU utilization and memory usage for all GPUs on the system using
+    nvidia-smi. Returns a list of GPU objects."""
+
+    try:
+        p = Popen(["nvidia-smi","--query-gpu=index,utilization.gpu,memory.total,memory.used", "--format=csv,noheader,nounits"], stdout=PIPE)
+        stdout, stderror = p.communicate()
+    except:
+        return []
+    output = stdout.decode('UTF-8')
+
+    # Parse output
+    lines = output.split(os.linesep)
+
+    numDevices = len(lines)-1
+    GPUs = []
+    for g in range(numDevices):
+        line = lines[g]
+        vals = line.split(', ')
+        for i in range(4):
+            if (i == 0):
+                deviceIds = int(vals[i])
+            elif (i == 1):
+                gpuUtil = safeFloatCast(vals[i])/100
+            elif (i == 2):
+                memTotal = safeFloatCast(vals[i])
+            elif (i == 3):
+                memUsed = safeFloatCast(vals[i])
+
+        GPUs.append(GPU(deviceIds, gpuUtil, memTotal, memUsed))
+    return GPUs
